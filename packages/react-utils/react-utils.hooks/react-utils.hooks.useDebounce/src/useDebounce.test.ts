38b0eed741785274249c7db4f21cbbdbec31ceb4
Guillaume CATEL <hello@guillaumecatel.com>
2022-05-27 13:56:39 +0200

    feat(packages/react-utils): add useDebounce hook

---
+++ packages/react-utils/react-utils.hooks/react-utils.hooks.useDebounce/src/useDebounce.test.ts
@@ -0,0 +1,7 @@
+import { useDebounce } from './useDebounce'
+
+describe('react-utils.hooks.useDebounce', () => {
+  it('should be defined', () => {
+    expect(useDebounce).toBeDefined()
+  })
+})
--- packages/react-utils/react-utils.hooks/react-utils.hooks.useDebounce/src/useDebounce.ts
+++ packages/react-utils/react-utils.hooks/react-utils.hooks.useDebounce/src/useDebounce.ts
@@ -1,4 +1,39 @@
-const useDebounce = () => undefined
+import React from 'react'
+import debounce from 'lodash.debounce'
 
+import type { DebouncedFunc, DebounceSettings } from 'lodash'
+
+/**
+ * Debounce hook
+ * Debounces a function
+ *
+ * @param callback The callback to debounce
+ * @param wait The duration to debounce
+ * @param options The options object.
+ * @param options.leading Specify invoking on the leading edge of the timeout.
+ * @param options.maxWait The maximum time func is allowed to be delayed before it’s invoked.
+ * @param options.trailing Specify invoking on the trailing edge of the timeout.
+ * @returns Returns the new debounced function.
+ */
+const useDebounce = <T extends (...args: any[]) => unknown>(
+  callback: T,
+  wait?: number,
+  options?: DebounceSettings
+) => {
+  const createDebouncedCallback = React.useCallback(
+    (fn: T): DebouncedFunc<T> => {
+      return debounce(fn, wait, options)
+    },
+    [wait, options]
+  )
+
+  const debouncedCallbackRef = React.useRef<DebouncedFunc<T>>(createDebouncedCallback(callback))
+
+  React.useEffect(() => {
+    debouncedCallbackRef.current = createDebouncedCallback(callback)
+  }, [callback, createDebouncedCallback])
+
+  return debouncedCallbackRef.current
+}
 export { useDebounce }
 export default useDebounce
